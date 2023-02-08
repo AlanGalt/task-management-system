186e315ea9bd6b4479272559941d3084190476aa
Aleksandr Andreenko <andreenko55@gmail.com>
2023-02-08 08:59:08 +0300

    Add task component

---
+++ Frontend/src/components/Task/Task.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+import { TaskProps } from './Task.types';
+
+const Task = ({ title, onRemove }: TaskProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [taskTitle, setTaskTitle] = useState(title);
+
+  function handleEditClick() {
+    setIsEditing(true);
+  }
+
+  function handleSaveClick() {
+    setIsEditing(false);
+  }
+
+  function handleTaskTitleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setTaskTitle(e.target.value);
+  }
+
+  return (
+    <div>
+      {isEditing ? (
+        <>
+          <input type="text" value={taskTitle} onChange={handleTaskTitleChange} />
+          <button onClick={handleSaveClick}>Save</button>
+        </>
+      ) : (
+        <>
+          <span>{taskTitle}</span>
+          <button onClick={handleEditClick}>Edit</button>
+          <button onClick={onRemove}>Remove</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Task;
