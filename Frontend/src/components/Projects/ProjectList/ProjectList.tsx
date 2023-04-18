0908cccabf7955a4afab990bf330b5ea0d9fb0a2
Aleksandr Andreenko <andreenko55@gmail.com>
2023-04-18 13:00:18 +0300

    Add route matching, add several handler functions for projects

--- Frontend/src/components/Projects/ProjectList/ProjectList.tsx
+++ Frontend/src/components/Projects/ProjectList/ProjectList.tsx
@@ -1,24 +1,28 @@
 import { useEffect, useState } from 'react';
 import { Route, Routes, Link, useLocation, matchPath } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { serverTimestamp } from 'firebase/firestore';
+import { serverTimestamp, Timestamp } from 'firebase/firestore';
+import { v4 as uuidv4 } from 'uuid';
 
 import Project from '../Project';
-import { ProjectData } from '../Project/Project.types';
+import { DefaultRole, LabelColor, ProjectData } from '../Project/Project.types';
 import CreateProjectPopover from '../CreateProjectPopover';
 import { auth } from '../../../App';
-import useProjects from '../../../hooks/useProjects';
+import useProjects from '../../../hooks/useProjects/useProjects';
 import NotFound from '../../../views/NotFound';
 import Loader from '../../Loader';
+import useDefaultRoles from '../../../hooks/useDefaultRoles';
+import { LabelData } from '../../Task/Task.types';
 
 const ProjectList = () => {
   const [openedProject, setOpenedProject] = useState<ProjectData | null>(null);
+  const [showNotFound, setShowNotFound] = useState(false);
+
   const [user] = useAuthState(auth as any);
-  // console.log(user);
-
-  const { projects, addProject, updateProject, deleteProject } = useProjects();
-
+  const { projects, addProject, updateProject, deleteProject, removeMember } = useProjects();
+  const defaultRoles = useDefaultRoles();
   const location = useLocation();
+
   useEffect(() => {
     if (projects) {
       matchCurrentPathToProject();
@@ -27,30 +31,44 @@ const ProjectList = () => {
 
   const matchCurrentPathToProject = () => {
     const match = matchPath({ path: '/dashboard/:projectId' }, location.pathname);
+    const currentProject = projects?.find((project) => project.id === match?.params.projectId);
+    const doesMatch = !!match && !!currentProject;
 
-    const currentProject = projects.find((project) => project.id === match?.params.projectId);
-
-    if (match && currentProject) {
+    if (doesMatch) {
       setOpenedProject(currentProject);
+      setShowNotFound(false);
+      return;
     }
+
+    setShowNotFound(true);
   };
 
   const handleAddProject = async (newProject: Partial<ProjectData>) => {
     if (!user) return;
+
     await addProject({
       ...newProject,
       creatorId: user.uid,
-      createdAt: serverTimestamp(),
+      createdAt: serverTimestamp() as Timestamp,
       active: true,
       members: [
         {
           email: user.email ?? 'default user email',
           name: user.displayName ?? 'default user name',
-          roleId: 'DEllHU6JjkL5iIKICuSY',
+          roleName: DefaultRole.Admin,
           uid: user.uid,
+          photoURL: user.photoURL ?? '',
         },
       ],
       membersUid: [user.uid],
+      roles: [],
+      labels: Object.values(LabelColor).map((color) => {
+        return {
+          id: uuidv4(),
+          color: color,
+          title: '',
+        } as LabelData;
+      }),
     });
   };
 
@@ -60,25 +78,23 @@ const ProjectList = () => {
   };
 
   return (
-    <div className={'flex justify-center h-full p-2'}>
+    <div className="flex justify-center h-full">
       <Routes>
         <Route
           path="/"
           element={
             <div className="flex flex-col items-center w-full">
-              <h2 className="text-2xl font-bold">Projects</h2>
-              <div className="flex flex-row items-center w-full gap-4 p-8">
+              <h2 className="mt-2 text-2xl font-bold">Projects</h2>
+              <div className="flex flex-row flex-wrap items-center w-full gap-4 p-8">
                 {!!projects?.length &&
                   projects.map((project) => (
                     <Link
                       key={project.id}
-                      className="h-32 px-4 py-2 overflow-hidden text-white break-words rounded-md w-52 bg-base-content hover:cursor-pointer"
+                      className="w-56 h-32 px-4 py-2 text-white rounded-md bg-base-content hover:cursor-pointer"
                       onClick={() => setOpenedProject(project)}
                       to={`${project.id}`}
                     >
-                      <div className="h-8 overflow-hidden text-lg break-words text-ellipsis whitespace-nowrap">
-                        {project.title}
-                      </div>
+                      <p className="h-8 text-lg truncate">{project.title}</p>
                     </Link>
                   ))}
                 <CreateProjectPopover onSubmit={handleAddProject} />
@@ -92,15 +108,19 @@ const ProjectList = () => {
             element={
               <Project
                 projectData={openedProject}
-                onDelete={handleDeleteProject}
-                onUpdate={updateProject}
+                defaultRoles={defaultRoles ?? []}
+                onDelete={() => handleDeleteProject(openedProject.id)}
+                onUpdate={(updatedData: Partial<ProjectData>) =>
+                  updateProject(openedProject.id, updatedData)
+                }
+                removeMember={(memberUid: string) => removeMember(openedProject.id, memberUid)}
               />
             }
           />
         )}
         <Route
           path="*"
-          element={projects.length ? <NotFound /> : <Loader className="w-full h-ful" />}
+          element={showNotFound ? <NotFound /> : <Loader className="w-full h-full" />}
         />
       </Routes>
     </div>
